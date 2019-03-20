1c58d4d9811bb0de121f0a82c11dec8f94649256
Shaun Hill <shaun.hill@gmail.com>
2019-03-20 02:01:48 -0400

    Finish working and refactor palindrome method

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 module.exports = Phrase;
 
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
-}
-
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
-}
-
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
@@ -20,23 +9,21 @@ String.prototype.reverse = function() {
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return string.toLowerCase();
-  }
-
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
-  }
-
-  // Makes the phrase LOUDER.
-  this.louder = function() {
-    return this.content.toUpperCase();
   }
 }
 
